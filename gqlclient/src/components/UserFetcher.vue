36f84d940fd541d4ae63eac97e5340b30214d5e3
Max Twardowski <twardowskimax@gmail.com>
2018-12-16 16:06:47 +0100

    Added userfetcher component for users{} query

--- gqlclient/src/components/UserFetcher.vue
+++ gqlclient/src/components/UserFetcher.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
     <h1>User fetcher</h1>
+    <button @click="handleFetch">Fetch users</button>
+    <p v-if="error" style="color:red">You need to be authenticated</p>
+    <ul>
+      <li v-for="user in usersData">
+        <p>ID: {{ user.id }}</p>
+        <p>Username: {{ user.username }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
-  name: 'UserFetcher'
+  name: 'UserFetcher',
+  data () {
+    return {
+      usersData: [],
+      error: undefined
+    }
+  },
+  methods: {
+    handleFetch () {
+      const usersQuery = gql`query {
+        users {
+          id
+          username
+        }
+      }`
+
+      this.$apollo.query({
+        query: usersQuery,
+        headers: {
+          authorization: localStorage.getItem('token')
+        }
+      }).then(res => {
+        this.usersData = res.data.users
+      }).catch(err => {
+        this.error = err
+      })
+    }
+  }
 }
 </script>
